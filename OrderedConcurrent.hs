58a6b72b11e2424d20f730f035f1a2fffd308677
Matthew Mirman <mmirman@andrew.cmu.edu>
2016-12-02 16:01:44 -0800

    added consumeordaslin

--- OrderedConcurrent.hs
+++ OrderedConcurrent.hs
@@ -37,6 +37,27 @@ class EQ (x::Nat) (y::Nat) (b::Bool) | x y -> b
 instance {-# OVERLAPPABLE #-} (b ~ False) => EQ x y b
 instance {-# OVERLAPPING #-} EQ x x True
 
+
+class ConsumeOrdAsLin (v::Nat) (i::[Cont]) (o::[Cont]) | v i -> o
+class ConsumeOrdAsLinHelperOrd (b::Bool) (v::Nat) (x::Nat) (i::[Cont]) (o::[Cont]) | b v x i -> o
+class ConsumeOrdAsLinHelper (b::Bool) (v::Nat) (x::Nat) (i::[Cont]) (o::[Cont]) | b v x i -> o
+class ConsumeOrdAsLinHelperReg (b::Bool) (v::Nat) (x::Nat) (i::[Cont]) (o::[Cont]) | b v x i -> o
+
+instance ConsumeOrdAsLin v i o                          => ConsumeOrdAsLin v (None:i) (None:o)
+instance (EQ v x b, ConsumeOrdAsLinHelperOrd b v x i o) => ConsumeOrdAsLin v (Om x: i)  o
+instance (EQ v x b, ConsumeOrdAsLinHelper b v x i o)    => ConsumeOrdAsLin v (Lin x: i) o
+instance (EQ v x b, ConsumeOrdAsLinHelperReg b v x i o) => ConsumeOrdAsLin v (Reg x: i) o
+
+instance                          ConsumeOrdAsLinHelperOrd True  v x i (None:i)
+instance ConsumeOrdAsLin v i o => ConsumeOrdAsLinHelperOrd False v x i (Om x:o)
+
+instance                          ConsumeOrdAsLinHelper True  v x i (None:i)
+instance ConsumeOrdAsLin v i o => ConsumeOrdAsLinHelper False v x i (Lin x:o)
+
+instance                          ConsumeOrdAsLinHelperReg True  v x i (None:i)
+instance ConsumeOrdAsLin v i o => ConsumeOrdAsLinHelperReg False v x i (Reg x:o)
+
+
 --
 -- Type level machinery for consuming a variable in a list of variables.
 --
@@ -88,32 +109,50 @@ class End (l :: [Cont]) (v :: Cont) (l' :: [Cont]) | l v -> l'
 instance End '[] a (a:'[])
 instance End a v2 b => End (v:a) v2 (v:b)
 
-class Start (l :: [Cont]) (v :: Cont) (l' :: [Cont]) | l v -> l'
-instance Start l v (v ': l)
 
-class Concat (a :: [Cont]) (b :: [Cont]) (c :: [Cont]) | a b -> c, c a -> b
-instance Concat '[] b b
-instance Concat a b c => Concat (h:a) b (h:c)
-
+class NotOrd (l :: [Cont]) (n :: Nat)
+instance NotOrd (Lin x:l) x
+instance NotOrd (Reg x:l) x
+instance (NotOrd l x) => NotOrd (None:l)  x
+instance (EQ x y False, NotOrd l x) => NotOrd (Om y:l)  x
+instance (EQ x y False, NotOrd l x) => NotOrd (Lin y:l) x
+instance (EQ x y False, NotOrd l x) => NotOrd (Reg y:l) x
 
 
 type OrdVar repr vid a = forall (v::Nat) (i::[Cont]) (o::[Cont]) . ConsumeOrd vid i o => repr v i o vid a 
 type LinVar repr vid a = forall (v::Nat) (i::[Cont]) (o::[Cont]) . ConsumeLin vid i o => repr v i o vid a
 type RegVar repr vid a = forall (v::Nat) (i::[Cont]) (o::[Cont]) . ConsumeReg vid i o => repr v i o vid a
 
+newtype C (vid::Nat) (hi::[Cont]) (ho::[Cont]) (x :: Nat) a = C { unC :: Chan a -> IO () }
 
-newtype a :->> b = ELolli {unELolli :: Chan (a, Chan b) }
-newtype a :-<> b = LLolli {unLLolli :: Chan (a, Chan b) }  
-newtype a :->  b = Lolli  {unLolli  :: Chan (a, Chan b) }
+newtype a :>-> b = SLolli {unSLolli :: (Chan b , Chan a -> IO ()) }
+newtype a :->> b = ELolli {unELolli :: (Chan b , Chan a -> IO ()) }
+newtype a :-<> b = LLolli {unLLolli :: (Chan b , Chan a -> IO ()) }
+newtype a :->  b = Lolli  {unLolli  :: (Chan b , Chan a -> IO ()) }
 
 class OrdSeq (repr :: Nat -> [Cont] -> [Cont] -> Nat -> * -> *) where
   type Name repr :: Nat -> [Cont] -> [Cont] -> Nat -> * -> *
 
+  -- no concurrency introduced
+  send :: (forall v . Name repr v hi hi w a) -- no context modifications at all ensures regularity
+       -> (forall v . Name repr v hi hi x (a :-> b))
+       -> (RegVar (Name repr) x b -> repr vid hi ho z c)
+       -> repr vid hi ho z c
+          
+  -- no concurrency introduced
+  recv :: (RegVar (Name repr) vid a -> repr (S vid) (Reg vid:hi) (Reg vid:ho) x b)
+       -> repr vid hi ho x (a :-> b)   
+
+          
+  lSend :: (NotOrd hi w, NotOrd hi x)
+        => (forall v . Name repr v hi hi2 w a)
+        -> (forall v . Name repr v hi2 ho x (a :-<> b))
+        -> (LinVar (Name repr) x b -> repr vid hi2 ho2 z c)
+        -> repr vid hi ho2 z c
+           
   lRecv :: (LinVar (Name repr) vid a -> repr (S vid) (Lin vid:hi)  (None:ho) x b)
        -> repr vid hi ho x (a :-<> b)
-          
-  recv :: (RegVar (Name repr) vid a -> repr (S vid) (Reg vid:hi) (Reg vid:ho) x b)
-      -> repr vid hi ho x (a :-> b)
+
           
   sSend :: (forall v . Name repr v hi hi2 w a) -- This can use non-ordered variables, but if they are ordered,
         -> (forall v . Name repr v hi2 ho x (a :>-> b)) -- it ensures that they are in fact ordered, as they come with a type constraint ensuring they are used this way.
@@ -123,17 +162,23 @@ class OrdSeq (repr :: Nat -> [Cont] -> [Cont] -> Nat -> * -> *) where
 
   sRecv :: (OrdVar (Name repr) y a -> repr (S y) (Om y:hi) (None:ho) x b)
         -> repr y hi ho x (a :>-> b)
+           
+  eSend :: ConsumeOrdAsLin w hi ho2    -- this case is strange as we need to make sure we eat w in ho2 since we never see w there
+        => (forall v . Name repr v hi hi2 x (a :->> b))
+        -> (forall v . Name repr v hi2 ho w a)
+        -> (OrdVar (Name repr) x b -> repr vid hi ho2 z c)
+        -> repr vid hi ho2 z c         
 
-newtype C (vid::Nat) (hi::[Cont]) (ho::[Cont]) (x :: Nat) a = C { unC :: Chan a -> IO () }
-
-newtype a :>-> b = SLolli {unSLolli :: (Chan b , Chan a -> IO ()) }
+  eRecv :: (End hi (Om y) hi', End ho None ho')
+        => (OrdVar (Name repr) y a -> repr (S y) hi' ho' x b)
+        -> repr y hi ho x (a :->> b)
+  
 
 instance OrdSeq C where
-  
   type Name C = C
 
-  sRecv (f :: OrdVar C y a -> C (S y) (Om y:hi) (None:ho) x b) = C $ \cxab -> do
-    (cb,ca_io) <- unSLolli <$> readChan cxab
+  sRecv (f :: OrdVar C y a -> C (S y) (Om y:hi) (None:ho) x b) = C $ \cab -> do
+    (cb,ca_io) <- unSLolli <$> readChan cab
     (unC $ f $ C ca_io) cb
 
   sSend (C ca_io) (C cab_io) procB_C = C $ unC $ procB_C $ C $ \cb -> do
@@ -141,11 +186,31 @@ instance OrdSeq C where
     forkIO $ cab_io cab
     writeChan cab $ SLolli (cb, ca_io)
 
+
+    
+
 evalC :: C Z '[] '[] chan a -> a -> IO ()
 evalC e a = do
   c <- newChan
-  forkIO $ unC e c
-  writeChan c a
+  writeChan c a  
+  unC e c
+
 
 tm :: (a :>-> b) :>-> (a :>-> b) -> IO ()
 tm = evalC $ sRecv $ \y -> sRecv $ \z -> sSend z y id
+
+
+tm2 :: b :>-> b -> IO ()
+tm2 = evalC $ sRecv id
+
+void x = x >> return ()
+
+main = do
+  a <- newChan
+  -- this doesn't block
+  tm2 $ SLolli (a, const $ putStrLn "ho")
+
+  b <- newChan
+  -- this blocks. 
+  tm $ SLolli (b, const $ putStrLn "hi")
+  
