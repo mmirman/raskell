cfb52d5534ba05b7a9ce4cf6d56f538963ead5af
Matthew Mirman <mmirman@andrew.cmu.edu>
2016-12-07 15:50:34 -0800

    without close

--- Control/Concurrent/Chan/Forwardable.hs
+++ Control/Concurrent/Chan/Forwardable.hs
@@ -1,55 +1,65 @@
-module Control.Concurrent.Chan.Forwardable ( Chan()
+module Control.Concurrent.Chan.Forwardable {- ( Chan()
                                            , newChan
                                            , writeChan
                                            , readChan
                                            , forwardChan
-                                           ) where
+                                           ) -} where
 
 import qualified Control.Concurrent.Chan.Unagi as U
 import Control.Concurrent.MVar
+import Data.IORef
+import Control.Concurrent (forkIO)
 
+type UChan a = (U.InChan a, U.OutChan a)
+type WIO a = Either a (IO a)
 
-type UnagiChan a = (U.InChan a, U.OutChan a)
-
-newtype Chan a = Chan { unChan :: (MVar (U.InChan a), MVar (U.OutChan a)) }
+newtype Chan a = Chan { unChan :: (MVar (U.InChan (WIO a)), IORef (U.OutChan (WIO a))) }
 
 
 newChan = do
   (ci,co) <- U.newChan
   mi <- newMVar ci
-  mo <- newMVar co 
-  return $ Chan (mi,mo)
+  to <- newIORef co
 
 
-writeChan (Chan (mi,mo)) v = withMVar mi $ \ci -> U.writeChan ci v
+  return $ Chan (mi,to)
+
+-- can't put a rwlock between these because both a rwRead/rwWrite on the readChan
+-- might block either the rwWrite/rwRead necessary to unblock it on the writeChan
+writeChan (Chan (mi,_)) v = withMVar mi $ \ci -> U.writeChan ci (Left v)
+
+
+-- can't put a rwlock between readChan and forwardChan because both a rwRead/rwWrite on the readChan
+-- might block either rwWrite/rwRead forwardChan which might open up a writeChan on the opposite
+-- channel
+readChan (Chan (mi,to)) = do
+  v <- readIORef to >>= U.readChan
+  case v of
+    Right b -> b
+    Left v -> return v
   
-readChan (Chan (mi,mo)) = do
-  co <- takeMVar mo
-  v <- U.readChan co
-  tryPutMVar mo co
-  return v
-
 -- how does this behave with multiple forwardings?
-forwardChan (Chan (mi,mo)) (Chan (mi',mo')) = do
+forwardChan :: forall a . Chan a -> Chan a -> IO ()    
+forwardChan c@(Chan (mi,to)) (Chan (mi',to')) = do
   ci' <- withMVar mi $ swapMVar mi'
-
-  -- it's safe to perform what is essentially a read here,
-  -- since we already placed c's input channel in c' so further
-  -- inputs on both channels will be mapped to here.
-  withMVar mo $ \co -> do
-    mco' <- tryTakeMVar mo'
-    case mco' of
-      Nothing -> do
-        -- we know somebody is reading "co'" and nothing will
-        -- ever write to "ci'" because we already performed the swap.
-        v <- U.readChan co
-        -- thusly, its safe to do a putMVar here since we know
-        -- that "tryPutMVar mo co" won't do anything after we've done this.
-        -- and that future "takeMVars mo'" are going to rely
-        -- on the new "co" rather than "ci'" so they are permitted to run
-        -- concurrently.
-        putMVar mo' co        
-        U.writeChan ci' v
+  
+  co' <- readIORef to'
+  atomicWriteIORef to' =<< readIORef to
+    
+  let getOldOrNew :: IO a
+      getOldOrNew = do
+        (v,_) <- U.tryReadChan co'
+        v <- U.tryRead v
+        case v of
+          Just (Left v) -> do
+            -- we know we just read from the chan co' because getOldOrNew was called
+            v' <- U.readChan co' -- thus we need to actually read it.
+            return v
+          _ -> do -- we know we aren't putting anything new on ci', so we need something off co
+            readChan c
         
-      Just co' -> do
-        putMVar mo' co
+  U.writeChan ci' $ Right getOldOrNew -- executes if we're stuck on a read.
+    
+
+void = (>> return ())
+
