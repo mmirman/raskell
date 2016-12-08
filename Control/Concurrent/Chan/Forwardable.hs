2fd46b3a6c26e2cade6ebf058f0cd5ec6799ee54
Matthew Mirman <mmirman@andrew.cmu.edu>
2016-12-07 23:05:48 -0800

    some cleaning on channels

--- Control/Concurrent/Chan/Forwardable.hs
+++ Control/Concurrent/Chan/Forwardable.hs
@@ -1,15 +1,14 @@
-module Control.Concurrent.Chan.Forwardable {- ( Chan()
+module Control.Concurrent.Chan.Forwardable ( Chan()
                                            , newChan
                                            , writeChan
                                            , readChan
                                            , forwardChan
-                                           ) -} where
+                                           ) where
 
 import qualified Control.Concurrent.Chan.Unagi as U
 import Data.IORef
 import Control.Concurrent (forkIO)
 import GHC.Base (join)
-type UChan a = (U.InChan a, U.OutChan a)
 
 newtype Chan a = Chan { unChan :: (IORef (U.InChan (IO a)), IORef (U.OutChan (IO a))) }
 
@@ -18,24 +17,17 @@ newChan = do
   (ci,co) <- U.newChan
   mi <- newIORef ci
   to <- newIORef co
-
-
   return $ Chan (mi,to)
 
--- can't put a rwlock between these because both a rwRead/rwWrite on the readChan
--- might block either the rwWrite/rwRead necessary to unblock it on the writeChan
+
+
 writeChan (Chan (mi,_)) v = do
   ci <- readIORef mi
   U.writeChan ci (return v)
 
-
--- can't put a rwlock between readChan and forwardChan because both a rwRead/rwWrite on the readChan
--- might block either rwWrite/rwRead forwardChan which might open up a writeChan on the opposite
--- channel
 readChan (Chan (mi,to)) = join $ readIORef to >>= U.readChan
 
   
--- how does this behave with multiple forwardings?
 forwardChan :: forall a . Chan a -> Chan a -> IO ()    
 forwardChan c@(Chan (mi,to)) (Chan (mi',to')) = do
   ci' <- readIORef mi
@@ -59,7 +51,6 @@ forwardChan c@(Chan (mi,to)) (Chan (mi',to')) = do
       useAll = readIfAvailable (\v -> writeChan c v >> useAll) $ return ()
         
   useAll
-  
   U.writeChan ci' $ getOldOrNew -- executes if we're stuck on a read.
     
 
